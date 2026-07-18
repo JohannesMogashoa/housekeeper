80af0eda962ff48494b0789da5b86d5f5c02f993
Johannes Mogashoa <yngstln.dev@gmail.com>
2026-07-18 22:26:25 +0200

    feat(hk-14): configure web API and session services

--- src/HouseKeeper.Web/Program.cs
+++ src/HouseKeeper.Web/Program.cs
@@ -1,4 +1,5 @@
 using HouseKeeper.Web;
+using HouseKeeper.Web.Services;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,6 +8,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string apiBaseUrl = builder.Configuration["ApiBaseUrl"]
+    ?? throw new InvalidOperationException("ApiBaseUrl configuration is required.");
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(apiBaseUrl, UriKind.Absolute)
+});
+builder.Services.AddScoped<DevelopmentSession>();
+builder.Services.AddScoped<HouseKeeperApiClient>();
 
 await builder.Build().RunAsync();
